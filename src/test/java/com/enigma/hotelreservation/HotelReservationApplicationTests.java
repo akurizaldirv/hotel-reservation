849d02698b8d521b5070dc1b10c6ad6721d681e8
zaldi <akurizaldirv@gmail.com>
2024-03-24 17:34:33 +0700

    [INIT] Initialize Project

---
+++ src/test/java/com/enigma/hotelreservation/HotelReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.hotelreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
